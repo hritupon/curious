64c626b6c033fa43c5bdd3c057647f0220a7d20c
hritupon <hritupon.saikia@gmail.com>
2020-02-01 15:52:08 +0530

    Adding the GeoHash project structure

---
+++ GeoHash/src/test/java/com/curious/GeoHash/GeoHashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curious.GeoHash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoHashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
